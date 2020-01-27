289a4a830ebf2376cd7b53b128760f1aa9f44ff6
stavrosgeorghiades <stavros.georghiades@gmail.com>
2020-01-27 19:11:31 +0200

    Implemented the chat app

---
+++ src/utils/messages.js
@@ -0,0 +1,19 @@
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        created_at: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        created_at: new Date().getTime()
+    }
+}
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
